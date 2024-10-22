aef74d52574581955fccbc83239a9e00a70cb557
Tae <65070089@kmitl.ac.th>
2024-10-22 13:28:09 +0700

    Added tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBlue: '#eceff7', // Light background color
+        darkBlue: '#1e3a8a', // Darker blue for dark mode (adjust as needed)
+      },
+      backgroundImage: {
+        'light-gradient': 'linear-gradient(152deg, rgba(236,239,247,1) 0%, rgba(246,255,253,1) 36%, rgba(252,250,255,1) 100%)',
+      },
+    },
   },
   plugins: [],
 }
