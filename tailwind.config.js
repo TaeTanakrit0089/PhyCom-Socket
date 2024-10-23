7bee02b3473d74523e2df516626bc7e0f087d329
Tae <65070089@kmitl.ac.th>
2024-10-23 18:09:49 +0700

    Updated Connection Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,6 @@ module.exports = {
       backgroundImage: {
         'light-gradient': 'linear-gradient(113deg, rgba(222,231,255,1) 0%, rgba(236,255,251,1) 51%, rgba(244,236,255,1) 100%)',
         'dark-gradient': 'linear-gradient(113deg, rgba(13,24,60,1) 0%, rgba(0,36,9,1) 51%, rgba(25,17,42,1) 100%)',
-
       },
     },
   },
