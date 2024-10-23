da8a5dd6ba1eb619dc84d4ac5607aa548516086b
Tae <65070089@kmitl.ac.th>
2024-10-23 10:00:43 +0700

    Added Copy Button

--- src/app/hive/questions/questions.component.ts
+++ src/app/hive/questions/questions.component.ts
@@ -1,16 +1,46 @@
 import {Component, Input} from '@angular/core';
+import {FaIconComponent, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {faClipboard, faCheck} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-questions',
   standalone: true,
-  imports: [],
   templateUrl: './questions.component.html',
-  styleUrl: './questions.component.css'
+  imports: [
+    FaIconComponent
+  ],
+  styleUrls: ['./questions.component.css']
 })
 export class QuestionsComponent {
-  @Input() student!: string[];
+  @Input() student: string[] = [];
+
+  // Track copied state for each topic
+  copiedState: { [key: string]: boolean } = {};
+
+  constructor(private library: FaIconLibrary) {
+    this.library.addIcons(faClipboard, faCheck);
+  }
 
   getStudentId(): string {
     return this.student && this.student.length > 0 ? this.student[0] : 'student_id';
+  }
+
+  copyToClipboard(topic: string): void {
+    const fullTopic = this.getStudentId() + topic;
+    navigator.clipboard.writeText(fullTopic).then(() => {
+      this.copiedState[topic] = true;
+
+      // Revert back to clipboard icon after 2 seconds
+      setTimeout(() => {
+        this.copiedState[topic] = false;
+      }, 1000);
+    }, (err) => {
+      console.error('Could not copy text: ', err);
+    });
+  }
+
+  // Determine the icon based on the state
+  getIcon(topic: string): string {
+    return this.copiedState[topic] ? 'check' : 'clipboard';
   }
 }
