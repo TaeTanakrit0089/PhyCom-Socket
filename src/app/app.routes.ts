aac20c2ee2181bedb6f943b3936837287ff69261
Tae <65070089@kmitl.ac.th>
2024-10-23 14:25:42 +0700

    Add exam path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import {HiveComponent} from './hive/hive.component';
 import {Error404Component} from './templates/error-404/error-404.component';
 
 export const routes: Routes = [
-  // {path: '', redirectTo: '/exam', pathMatch: 'full'}, // Default route redirect to /exam
-  // {path: 'exam', component: HiveComponent},
-  {path: '', component: HiveComponent},
+  {path: '', redirectTo: '/exam', pathMatch: 'full'}, // Default route redirect to /exam
+  {path: 'exam', component: HiveComponent},
+  // {path: '', component: HiveComponent},
   {path: 'error', component: Error404Component},
   {path: '**', component: Error404Component}
 ];
