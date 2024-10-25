367b4f103153aaf310514e3c0e224d42f54756dc
Tae <65070089@kmitl.ac.th>
2024-10-25 22:03:11 +0700

    Improved NavBar CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,15 @@ module.exports = {
       colors: {
         lightBlue: '#eceff7', // Light background color
         darkBlue: '#1e3a8a', // Darker blue for dark mode (adjust as needed)
+        navBlue: '#2C538FE0',
+        navOrange: '#E89837E0'
       },
       backgroundImage: {
         'light-gradient': 'linear-gradient(113deg, rgba(222,231,255,1) 0%, rgba(236,255,251,1) 51%, rgba(244,236,255,1) 100%)',
         'dark-gradient': 'linear-gradient(113deg, rgba(13,24,60,1) 0%, rgba(0,36,9,1) 51%, rgba(25,17,42,1) 100%)',
+      },
+      borderRadius: {
+        'nav': '6px',
       },
     },
   },
