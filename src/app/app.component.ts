18bbdd611d0677ceb9a6722a1f05942f9b828cc4
Jarukrit Sripaploen <jarukritsripaploen@protonmail.com>
2024-10-26 03:37:12 +0700

    Use Angular Router instead of href

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
-import {RouterModule, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterModule, RouterOutlet} from '@angular/router';
 import {NavbarComponent} from './templates/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NavbarComponent],
+  imports: [RouterOutlet, RouterModule, NavbarComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
-  selector: 'app-home-page',
+  selector: "app-home-page",
   standalone: true,
-  imports: [],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: "./home-page.component.html",
+  styleUrl: "./home-page.component.css",
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
