367c2707c363cf795221340cd2286d8642e0bc13
Tae <65070089@kmitl.ac.th>
2024-10-26 12:58:39 +0700

    Improved Home page css and Added about components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,14 @@ import {ExamComponent} from './exam/exam.component';
 import {Error404Component} from './templates/error-404/error-404.component';
 import {MqttDashboard} from './mqtt/mqtt.component';
 import {HomePageComponent} from './home-page/home-page.component';
+import {AboutComponent} from './about/about.component';
 
 export const routes: Routes = [
   // {path: '', redirectTo: '/exam', pathMatch: 'full'}, // Default route redirect to /exam
   {path: '', component: HomePageComponent},
   {path: 'exam', component: ExamComponent},
   {path: 'mqtt', component: MqttDashboard},
-  // {path: '', component: ExamComponent},
-  {path: 'error', component: Error404Component},
+  {path: 'about', component: AboutComponent},
   {path: '**', component: Error404Component}
 ];
 
